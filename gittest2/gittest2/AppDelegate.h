5a5b6c56a290a3d2025674929bca014a6903398b
huang <huangwenlong@huangwenlongdeMacBook-Pro.local>
2015-08-30 19:00:31 +0800

    add

---
+++ gittest2/gittest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest2
+//
+//  Created by huangwenlong on 15/8/30.
+//  Copyright (c) 2015年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest2/gittest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest2
+//
+//  Created by huangwenlong on 15/8/30.
+//  Copyright (c) 2015年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
